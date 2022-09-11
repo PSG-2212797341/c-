a86bfcb42c7849f5fde1b32b0acde4d7e49bff39
PSG-2212797341 <111972750+PSG-2212797341@users.noreply.github.com>
2022-09-11 22:46:30 +0800

    Add files via upload

--- array.c
+++ array.c
@@ -331,3 +331,151 @@ int main()          //主函数
     text();
     return 0;
 } */
+
+// 2、扫雷：
+/* void menu()
+{
+    printf("菜单：\n");
+    printf("0、开始游戏\n");
+    printf("1、退出游戏\n");
+}
+
+void InitBoard(char board[ROWS][COLS],int rows,int cols,char set)       //初始化数组。
+{
+    int i = 0;
+    int j = 0;
+    for ( i = 0; i < rows; i++)
+    {
+        for ( j = 0; j < cols; j++)
+        {
+            board[i][j] = set;
+        }     
+    }   
+}
+
+void DisplayBoard(char board[ROWS][COLS],int row,int col)       //打印雷区
+{
+    int i = 0;
+    int j = 0;
+    for ( i = 0; i <= col; i++)             //打印列号。
+    {
+        printf("%d ",i);
+    }
+    printf("\n");
+    for ( i = 1; i <= row; i++)
+    {
+        printf("%d ",i);                    //打印行号。
+        for ( j = 1; j <= col; j++)
+        {
+            printf("%c ",board[i][j]);
+        }
+        printf("\n");     
+    }
+}
+
+void SetMine(char board[ROWS][COLS],int row,int col)            //在雷区随机放雷。
+{
+    int count = EASY_COUNT;
+    while (count)
+    {
+        int x = rand() % row + 1;
+        int y = rand() % col + 1;
+        if (board[x][y] == '0')
+        {
+            board[x][y] = '1';
+            count--;
+        }
+    } 
+}
+
+int get_mine_count(char mine[ROWS][COLS],int x,int y)       //计算mine[x][y]旁边的九个格子有几个雷。
+{                                                           //方法：把九个字符都加起来，最后一起减去八个字符零就得到几个雷了。
+    return mine[x - 1][y] +
+           mine[x - 1][y - 1] +
+           mine[x][y - 1] +
+           mine[x + 1][y - 1] +
+           mine[x + 1][y] +
+           mine[x + 1][y + 1] +
+           mine[x][y + 1] +
+           mine[x - 1][y + 1] -
+           8 * '0';
+}
+
+void FindMine(char mine[ROWS][COLS],char show[ROWS][COLS],int row,int col)      //扫雷函数。
+{
+    int x = 0;
+    int y = 0;
+    int win = 0;                            //使游戏能够结束（当所有雷都被找到时）。
+    while (win < row*col-EASY_COUNT)        //判断：以为每次找到一个安全的地方win都会自增一下，直到所有安全的地方都找到后，剩下的就是雷了。
+    {
+        printf("请输入排雷坐标：");
+        scanf("%d%d",&x,&y);
+        printf("\n");
+        if (x >= 1 && x <= row || y >= 1 && y<= col)        //判断输入位置的合法性。
+        {
+            if (mine[x][y] == '1')                  //如果玩家选到的地方是雷。
+            {
+                printf("很遗憾,你被炸死了\n");
+                DisplayBoard(mine,row,col);
+                break;
+            }
+            else
+            {
+                int count = get_mine_count(mine,x,y);           //如果不是，那么就返回以这个格子为中心旁边几个格子雷的情况。
+                show[x][y] = count + '0';                       //规律：任意数字字符（以1为例）：'1' - '0' = 1;
+                DisplayBoard(show,row,col);
+                win++;
+            }
+        }
+        else
+        {
+            printf("输入坐标错误,请重新输入\n");
+        }
+    }
+    if (win == row*col-EASY_COUNT)      //判断：以为每次找到一个安全的地方win都会自增一下，直到所有安全的地方都找到后，剩下的就是雷了，此时游戏结束。
+    {
+        printf("恭喜你,排雷成功");
+    }    
+}
+
+void game()
+{
+    char mine[ROWS][COLS] = { 0 };          //布置好的雷的信息。
+    char show[ROWS][COLS] = { 0 };          //排查出的雷的信息。
+    InitBoard(mine,ROWS,COLS,'0');          //初始化数组。
+    InitBoard(show,ROWS,COLS,'*');             
+    DisplayBoard(show,ROW,COL);             //打印棋盘。
+    SetMine(mine,ROW,COL);                  //设置雷的位置。
+    FindMine(mine,show,ROW,COL);            //扫雷。
+}
+
+void test()
+{
+    int input = 0;
+    srand((unsigned int) time(NULL));       //使用随机数函数之前，需要调用srand()函数，不然会报错；而里面的随机数起点的参数由time()函数实现，但是time()函数里面的参数类型是time_t类型，所以需要unsigned int强制转换为整形类型来使用。
+    do
+    {
+        menu();
+        printf("\n");
+        printf("请选择：");
+        scanf("%d",&input);
+        switch (input)
+        {
+        case 0:
+            game();
+            break;
+        case 1:
+            printf("游戏已退出\n");
+            break;
+        default:
+            printf("输入错误，请重新输入\n");
+            break;
+        }
+    } while (input);
+}
+
+int main()
+{
+    test();
+    return 0;
+} */
--- array.h
+++ array.h
@@ -1,9 +1,24 @@
-#define ROW 3
-#define COL 3
+// 三子棋头文件：
+// #define ROW 3
+// #define COL 3
 
-void InitBoard(char board[ROW][COL],int rwo,int col);
-void DisplayBoard(char board[ROW][COL],int row,int col);    
-void PlayerMove(char board[ROW][COL],int row,int col);
-void ComputerMove(char board[ROW][COL],int row,int col);
-char IsWin(char board[ROW][COL],int row,int col);
-int IsFair(char board[ROW][COL],int row,int col);
+// void InitBoard(char board[ROW][COL],int rwo,int col);
+// void DisplayBoard(char board[ROW][COL],int row,int col);    
+// void PlayerMove(char board[ROW][COL],int row,int col);
+// void ComputerMove(char board[ROW][COL],int row,int col);
+// char IsWin(char board[ROW][COL],int row,int col);
+// int IsFair(char board[ROW][COL],int row,int col);
+
+// 扫雷头文件：
+#define ROW 9
+#define COL 9
+
+#define ROWS ROW+2
+#define COLS COL+2
+
+#define EASY_COUNT 10
+
+void InitBoard(char board[ROWS][COLS],int rows,int cols,char set);              //初始化。
+void DisplayBoard(char board[ROWS][COLS],int row,int col);                      //打印
+void SetMine(char board[ROWS][COLS],int row,int col);                           //放雷。
+void FindMine(char mine[ROWS][COLS],char show[ROWS][COLS],int row,int col);     //扫雷。
